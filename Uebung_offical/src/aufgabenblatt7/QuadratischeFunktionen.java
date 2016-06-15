c3324ed9bc5e84d30b9f89eab31f9508bdd7086c
Johannes Kruber <johannes.kruber@haw-hamburg.de>
2016-06-15 16:43:46 +0200

    -.-

--- Uebung_offical/src/aufgabenblatt7/QuadratischeFunktionen.java
+++ Uebung_offical/src/aufgabenblatt7/QuadratischeFunktionen.java
@@ -3,7 +3,6 @@
  * @author Johannes Kruber, Michel Gerlach
  */
 package aufgabenblatt7;
-import aufgabenblatt7.NullstellenException;
 
 /**
  * Erstellt eine Quadratische Funktion zweiten Grades
@@ -25,13 +24,8 @@ public class QuadratischeFunktionen implements IFunktion
 	 * ergebnis
 	 */
 	@Override
-	public double berechnefVonX(double xWert) throws NullstellenException
+	public double berechnefVonX(double xWert)
 	{
-		if (this.a == 0 && this.b == 0 && this.c == 0)
-		{
-			throw new NullstellenException(
-					"Hoechstens zwei Werte der Funktion duerfen Null sein!");
-		}
 		double ergebnis = a * (xWert*xWert) + b * xWert + c;// 2x²+3x-2
 		return ergebnis;
 	}
@@ -41,13 +35,7 @@ public class QuadratischeFunktionen implements IFunktion
 	 */
 	@Override
 	public double berechneAbleitungFvonX(double xWert)
-			throws NullstellenException
 	{
-		if (this.a == 0 && this.b == 0)
-		{
-			throw new NullstellenException(
-					"Hoechstens ein Wert der Funktion darf Null sein!");
-		}
 		double ergebnis = (2 * a) * xWert + b;// 4x+3
 		return ergebnis;
 	}
